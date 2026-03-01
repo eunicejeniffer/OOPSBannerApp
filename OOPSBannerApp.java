230e7e99dbea0e4143fbacbfeb792c4ea42558db
eunicejeniffer <ej7376@srmist.edu.in>
2026-03-01 20:57:36 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,23 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+        printBanner();
+    }
 
-        System.out.println("  *****     *****     *******    *****  ");
-        System.out.println(" *     *   *     *   *      *   *     * ");
-        System.out.println(" *     *   *     *   *      *   *       ");
-        System.out.println(" *     *   *     *    *******     *****  ");
-        System.out.println(" *     *   *     *   *              *    ");
-        System.out.println(" *     *   *     *   *        *     *    ");
-        System.out.println("  *****     *****    *         *****     ");
+    public static void printBanner() {
 
+        String[] banner = {
+                "  *****     *****     *******    *****  ",
+                " *     *   *     *   *      *   *     * ",
+                " *     *   *     *   *      *   *       ",
+                " *     *   *     *    *******     *****  ",
+                " *     *   *     *   *              *    ",
+                " *     *   *     *   *        *     *    ",
+                "  *****     *****    *         *****     "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
