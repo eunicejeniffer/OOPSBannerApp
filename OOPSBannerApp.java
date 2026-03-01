fd00a3396e16580b289a095a98640138694298db
eunicejeniffer <ej7376@srmist.edu.in>
2026-03-01 20:58:41 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,21 @@
+import java.util.HashMap;
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****     *****     *******    *****  ");
-        System.out.println(" *     *   *     *   *      *   *     * ");
-        System.out.println(" *     *   *     *   *      *   *       ");
-        System.out.println(" *     *   *     *    *******     *****  ");
-        System.out.println(" *     *   *     *   *              *    ");
-        System.out.println(" *     *   *     *   *        *     *    ");
-        System.out.println("  *****     *****    *         *****     ");
+        HashMap<Integer, String> banner = new HashMap<>();
 
+        banner.put(1, "  *****     *****     *******    *****  ");
+        banner.put(2, " *     *   *     *   *      *   *     * ");
+        banner.put(3, " *     *   *     *   *      *   *       ");
+        banner.put(4, " *     *   *     *    *******     *****  ");
+        banner.put(5, " *     *   *     *   *              *    ");
+        banner.put(6, " *     *   *     *   *        *     *    ");
+        banner.put(7, "  *****     *****    *         *****     ");
+
+        for (int i = 1; i <= banner.size(); i++) {
+            System.out.println(banner.get(i));
+        }
     }
 }
