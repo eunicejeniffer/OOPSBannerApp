29eba7254c5ea19a03c1ce0050ed91fd59e800e9
eunicejeniffer <ej7376@srmist.edu.in>
2026-03-03 13:07:49 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,70 @@
-public class OOPSBannerApp {
+/**
+ * OOPSBannerApp UC6 – Static Functions for Character Patterns
+ * 
+ * UC6 improves modularity by creating reusable methods to generate
+ * ASCII patterns for O, P, and S instead of hardcoding inline strings.
+ */
+package UC6;
+public class OOPSBannerAppUC6 {
 
-    public static void main(String[] args) {
-        printBanner();
+    // Static method to return pattern for O
+    public static String[] getO() {
+        return new String[]{
+            "*****",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*****"
+        };
     }
 
-    public static void printBanner() {
+    // Static method to return pattern for P
+    public static String[] getP() {
+        return new String[]{
+            "*****",
+            "*   *",
+            "*   *",
+            "*****",
+            "*",
+            "*",
+            "*"
+        };
+    }
 
-        String[] banner = {
-                "  *****     *****     *******    *****  ",
-                " *     *   *     *   *      *   *     * ",
-                " *     *   *     *   *      *   *       ",
-                " *     *   *     *    *******     *****  ",
-                " *     *   *     *   *              *    ",
-                " *     *   *     *   *        *     *    ",
-                "  *****     *****    *         *****     "
+    // Static method to return pattern for S
+    public static String[] getS() {
+        return new String[]{
+            "*****",
+            "*   *",
+            "*",
+            "*****",
+            "    *",
+            "*   *",
+            "*****"
+        };
+    }
+
+    public static void main(String[] args) {
+
+        // Build banner using static functions
+        String[][] banner = {
+                getO(),
+                getO(),
+                getP(),
+                getS()
         };
 
-        for (String line : banner) {
+        System.out.println("OOPS Banner App - UC6 Static Functions");
+        System.out.println("--------------------------------------");
+
+        // Print the banner row-by-row
+        for (int row = 0; row < 7; row++) {
+            String line = "";
+            for (String[] letter : banner) {
+                line += letter[row] + "   ";
+            }
             System.out.println(line);
         }
     }
