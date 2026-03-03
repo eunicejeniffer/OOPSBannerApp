c9d8890548006203b6acef864a6eadbbd44aa61b
eunicejeniffer <ej7376@srmist.edu.in>
2026-03-03 13:05:57 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,45 @@
-public class OOPSBannerApp {
+public class OOPSBannerUC3 {
 
     public static void main(String[] args) {
 
-        String banner = String.join("\n",
-                "  *****     *****     *******    *****  ",
-                " *     *   *     *   *      *   *     * ",
-                " *     *   *     *   *      *   *       ",
-                " *     *   *     *    *******     *****  ",
-                " *     *   *     *   *              *    ",
-                " *     *   *     *   *        *     *    ",
-                "  *****     *****    *         *****     "
-        );
+        // Line patterns for each character using String.join(" ", ...)
+        String[] O = {
+                String.join(" ", " ***** "),
+                String.join(" ", "*     *"),
+                String.join(" ", "*     *"),
+                String.join(" ", "*     *"),
+                String.join(" ", "*     *"),
+                String.join(" ", "*     *"),
+                String.join(" ", " ***** ")
+        };
 
-        System.out.println(banner);
+        String[] P = {
+                String.join(" ", "****** "),
+                String.join(" ", "*     *"),
+                String.join(" ", "*     *"),
+                String.join(" ", "****** "),
+                String.join(" ", "*       "),
+                String.join(" ", "*       "),
+                String.join(" ", "*       ")
+        };
+
+        String[] S = {
+                String.join(" ", " ***** "),
+                String.join(" ", "*     *"),
+                String.join(" ", "*       "),
+                String.join(" ", " ***** "),
+                String.join(" ", "      *"),
+                String.join(" ", "*     *"),
+                String.join(" ", " ***** ")
+        };
+
+        // Display banner rows by combining corresponding lines of O O P S
+        System.out.println("OOPS Banner App - UC3: Render Using String.join()");
+        System.out.println("------------------------------------------------");
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(
+                String.join("   ", O[i], O[i], P[i], S[i])  // 3 spaces between characters
+            );
+        }
     }
-}
