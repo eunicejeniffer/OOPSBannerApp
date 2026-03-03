062910106d97724188b0630917c1d2c92e15a702
eunicejeniffer <ej7376@srmist.edu.in>
2026-03-03 13:06:38 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,52 @@
-public class OOPSBannerApp {
+public class OOPSBannerUC4 {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-                "  *****     *****     *******    *****  ",
-                " *     *   *     *   *      *   *     * ",
-                " *     *   *     *   *      *   *       ",
-                " *     *   *     *    *******     *****  ",
-                " *     *   *     *   *              *    ",
-                " *     *   *     *   *        *     *    ",
-                "  *****     *****    *         *****     "
+        // O pattern
+        String[] O = {
+            String.join(" ", " ***** "),
+            String.join(" ", "*     *"),
+            String.join(" ", "*     *"),
+            String.join(" ", "*     *"),
+            String.join(" ", "*     *"),
+            String.join(" ", "*     *"),
+            String.join(" ", " ***** ")
         };
 
-        for (String line : banner) {
+        // P pattern
+        String[] P = {
+            String.join(" ", "****** "),
+            String.join(" ", "*     *"),
+            String.join(" ", "*     *"),
+            String.join(" ", "****** "),
+            String.join(" ", "*       "),
+            String.join(" ", "*       "),
+            String.join(" ", "*       ")
+        };
+
+        // S pattern
+        String[] S = {
+            String.join(" ", " ***** "),
+            String.join(" ", "*     *"),
+            String.join(" ", "*       "),
+            String.join(" ", " ***** "),
+            String.join(" ", "      *"),
+            String.join(" ", "*     *"),
+            String.join(" ", " ***** ")
+        };
+
+        // Combine O O P S arrays into a single 2D array
+        String[][] banner = { O, O, P, S };
+
+        System.out.println("OOPS Banner App - UC4 Using Arrays & Loops");
+        System.out.println("-------------------------------------------");
+
+        // Print banner using loops
+        for (int row = 0; row < 7; row++) {
+            String line = "";
+            for (String[] letter : banner) {
+                line += letter[row] + "   ";  // spacing between letters
+            }
             System.out.println(line);
         }
     }
